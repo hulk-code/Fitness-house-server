70d17bc7aa4799d3acf13f86240140bed9ed927d
Akash <shoaibsarkarakash019@gmail.com>
2023-11-27 15:30:10 +0600

    create middle ware

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors=require('cors')
 const app = express()
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
@@ -43,7 +44,41 @@ async function run() {
 
 
 
-    
+    app.post('/jwt', async(req , res) =>{
+      const user=req.body;
+      const token=jwt.sign(user , process.env.ACCESS_TOKEN_SECRET ,{
+        expiresIn:'1hr'
+      });
+      res.send({token})
+    })
+
+    const verifytoken=(req , res , next)=>{
+      console.log('inside vasify token' , req.headers.authorization)
+      if(!req.headers.authorization){
+        return res.status(401).send({message : 'forbidden access'})
+      }
+      const token=req.headers.authorization.split(' ')[1]
+      jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err ,decoded) =>{
+        if(err){
+          return res.status(401).send({message:"forbodden Access"})
+        }
+        req.decoded =decoded;
+        next();
+      })
+     
+     }
+
+     const verifyAdmin=async(req  , res ,next)=>{
+      const email=req.decoded.email;
+      const query={email : email}
+      const user=await userCollection.findOne(query)
+      const isAdmin=user?.role==='admin';
+      if(!isAdmin){
+        return res.status(403).send({message:"forbidden access"})
+      }
+      next();
+     }
+
     app.get('/featured' ,async (req , res) =>{
         const result = await FeaturedCollection.find().toArray();
       res.send(result);
@@ -88,11 +123,12 @@ async function run() {
      res.send(result)
    })
 
-   app.get('/users' ,async (req , res) =>{
+   app.get('/users' , verifytoken,verifyAdmin, async (req , res) =>{
+    console.log(req.headers);
       const result = await userCollection.find().toArray();
     res.send(result);
   })
-  app.patch('/users/admin/:id' , async(req , res) =>{
+  app.patch('/users/admin/:id' , verifytoken,verifyAdmin, async(req , res) =>{
     const id=req.params.id
     const filter={_id :new ObjectId(id)}
     const updatedDoc={
@@ -103,6 +139,20 @@ async function run() {
     const result=await userCollection.updateOne(filter,updatedDoc)
     res.send(result)
    })
+   app.get('/users/admin/:email',verifytoken, async(req ,res) =>{
+    const email=req.params.email;
+    if(email !==req.decoded.email){
+     return res.status(403).send({message:"unauthorize access"})
+    }
+    const query={email :email};
+    const user=await userCollection.findOne(query)
+    let admin=false;
+    if(user){
+     admin=user?.role==="admin";
+    
+    }
+    res.send({admin})
+    })
    app.post('/users', async (req, res) => {
       const user = req.body;
       
@@ -114,21 +164,25 @@ async function run() {
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
+
     app.post('/beatrainer', async (req, res) => {
       const reqInfo = req.body;
       const result = await beATrainercollection.insertOne(reqInfo);
       res.send(result);
     });
+
     app.get('/routine' ,async (req , res) =>{
       const result = await classRoutine.find().toArray();
     res.send(result);
   })
+
   app.get('/routine/:id' , async(req ,res)=>{
     const id=req.params.id;
     const query={_id :new ObjectId(id)}
     const result=await classRoutine.findOne(query)
      res.send(result)
    })
+
    app.post('/routine', async (req, res) => {
     const routineInfo = req.body;
     const result = await classRoutine.insertOne(routineInfo);
