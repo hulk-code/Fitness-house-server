8c98738eb174bb3dcf036c02be0e82995fb9fa20
Akash <shoaibsarkarakash019@gmail.com>
2023-11-27 12:28:44 +0600

    make user admin

--- index.js
+++ index.js
@@ -88,6 +88,21 @@ async function run() {
      res.send(result)
    })
 
+   app.get('/users' ,async (req , res) =>{
+      const result = await userCollection.find().toArray();
+    res.send(result);
+  })
+  app.patch('/users/admin/:id' , async(req , res) =>{
+    const id=req.params.id
+    const filter={_id :new ObjectId(id)}
+    const updatedDoc={
+      $set:{
+        role:'admin'
+      }
+    }
+    const result=await userCollection.updateOne(filter,updatedDoc)
+    res.send(result)
+   })
    app.post('/users', async (req, res) => {
       const user = req.body;
       
